f4bc1d622d4098095db2dc53afa10e7a24d98f8f
Kevin Noonan <KJNoonan52@gmail.com>
2015-03-28 17:14:36 +0000

    Added index controller code

---
+++ app/controllers/api/v1/bars_controller.rb
@@ -0,0 +1,12 @@
+class Api::V1::BarsController < ApplicationController 
+  respond_to :json
+  def index
+  render :text => @lat1.inspect
+  @bars = Bar.where("lat >= :lat0 AND lat <= :lat1 AND lon >= :lon0 AND lon <= :lon1", { lat0: params[:lat0], 
+		lat1: params[:lat1], 
+		lon0: params[:lon0], 
+		lon1: params[:lon1]})
+  
+  render :json => @bars.all and return
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,17 @@ Rails.application.routes.draw do
   resources :specials
 
   resources :bars
-
+  
+  namespace :api do
+    namespace :v1 do
+       resources :bars, except: [:new, :edit, :delete]
+    end
+  end
+end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -59,4 +64,4 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
+
