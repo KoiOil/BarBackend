f694a8f98c37137fc5b2022cb28c92aaecb8c40c
Kevin Noonan <KJNoonan52@gmail.com>
2015-03-28 23:07:54 +0000

    Adds special_controller

--- app/controllers/api/v1/specials_controller.rb
+++ app/controllers/api/v1/specials_controller.rb
@@ -2,8 +2,8 @@ class Api::V1::SpecialsController < ApplicationController
   respond_to :json
   def index
     time = Time.new
-    puts time.strftime("%A")
-    @special = Special.where('id IN(SELECT specialid FROM dows WHERE dows.sid)'
-    render :json => @special.all and return
+    @special = Special.where(
+		["specials.barid = ?",params[:barid]]) 
+    render :json => @special.all and return	
   end
 end
--- app/models/dow.rb
+++ app/models/dow.rb
@@ -1,3 +1,3 @@
 class Dow < ActiveRecord::Base
-  belongs_to :special
+  belongs_to :special, :foreign_key => "specialid"
 end
