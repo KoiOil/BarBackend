62050701b79952292e719d5d47266222fea18da2
Kevin Noonan <KJNoonan52@gmail.com>
2015-03-29 04:42:25 +0000

    Changes the controllers

--- app/controllers/api/v1/bars_controller.rb
+++ app/controllers/api/v1/bars_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::BarsController < ApplicationController 
+  skip_before_filter  :verify_authenticity_token
   respond_to :json
-  def index
+  def create
   @bars = Bar.where("lat >= :lat0 AND lat <= :lat1 AND lon >= :lon0 AND lon <= :lon1", { lat0: params[:lat0], 
 		lat1: params[:lat1], 
 		lon0: params[:lon0], 
--- app/controllers/api/v1/specials_controller.rb
+++ app/controllers/api/v1/specials_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::SpecialsController < ApplicationController
+  skip_before_filter  :verify_authenticity_token
   respond_to :json
-  def index
+  def create
     time = Time.new
     @special = Special.where(
 		["specials.barid = ?",params[:barid]]) 
